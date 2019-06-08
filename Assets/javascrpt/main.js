08fb4be43fa7aff0d658bbc4f3c54affa183c7ad
vikctoria stephenson <vicstephjackson@gmail.com>
2019-06-08 13:41:52 -0700

    linking Firebase

--- Assets/javascrpt/main.js
+++ Assets/javascrpt/main.js
@@ -1,3 +1,35 @@
 // $(document).on('click', "#googlemapIcon", function(){
 //     var googlemapicon.
-// })
+// firebase username, password, sumbit storage, check authentication
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAEuerlOLYMOBNogqwF-yGw87MD3DEBrHo",
+    authDomain: "first-group-project-64a3b.firebaseapp.com",
+    databaseURL: "https://first-group-project-64a3b.firebaseio.com",
+    projectId: "first-group-project-64a3b",
+    storageBucket: "first-group-project-64a3b.appspot.com",
+    messagingSenderId: "362226278803",
+    appId: "1:362226278803:web:f676ad9e2157d83e"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  console.log(firebase);
+
+  const database = firebase.database();
+  const ref = database.ref('username/password');
+  const data = {
+    name: "victoria",
+    password: 1234
+  }
+  ref.push(data);
+   
+
+// $(document) firebase stores user information//
+
+  
+  
+
+
+
